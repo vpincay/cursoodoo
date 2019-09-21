8374fcd106e823381043ff5801000f16e8a2a358
vpincay <roysxp@gmail.com>
2019-09-21 00:12:34 -0300

    no message

--- saldoapp/models/models.py
+++ saldoapp/models/models.py
@@ -2,22 +2,34 @@
 
 from odoo import models, fields, api
 
+
+"""
+Clase models.Model
+===================
+Python --> SQL
+create() --> Insert
+write() --> update
+unlink()  --> delete
+search(),browse() --> select *
+"""
+
 class Movimiento(models.Model):
-    _name = 'sa.movimiento' # Nombre de la base de batos
-    _description = 'Movimiento' # Nombre del modelo en Odoo
-    
-    name = fields.Char(string="Concepto", size=150, index=True)
+    _name = "sa.movimiento" # Nombre de la base de datos sa_movimiento (SQL)
+    _description = "Movimiento" # Nombre del modelo en Odoo 'Movimiento' 
+
+    name = fields.Char(string="Concepto",size=150,index = True)
     #monto = fields.Char("Monto")
-    monto_total = fields.Float(string="Monto")
-    tipo = fields.Selection(string="Tipo", selection=[("I","Ingreso"),("E","Egreso")])
-    fecha = fields.Date(string="Fecha operación")
-    moneda = fields.Selection(string="Moneda", selection=[("PEN","Soles"), ("USD","Dolares")])
+    monto_total = fields.Float(string = "Monto")
+    tipo = fields.Selection(string="Tipo",selection=[('I','Ingreso'),("E","Egreso")])
+    fecha = fields.Date(string="Fecha de Operción")
+    moneda = fields.Selection(string="Moneda",selection=[("PEN","Soles"),("USD","Dólares")])
+
 
 class Categoria(models.Model):
     _name = "sa.categoria"
     _description = "Categoria"
-    _rec_name= "nombre"
+    _rec_name = "nombre"
 
     nombre = fields.Char(string="Nombre")
-    tipo = fields.Selection(string="Tipo",selection=[("I","Ingreso"),("E","Egreso")])
-    activo = fields.Boolean(string="Activo", default=True)
+    tipo = fields.Selection(string="Tipo",selection=[('I','Ingreso'),("E","Egreso")])
+    active = fields.Boolean(string="Activo",default=True)
