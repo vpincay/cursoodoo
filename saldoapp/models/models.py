34bbaacbcb0bbba8c3484b33e617b32e6a13d3cc
vpincay <roysxp@gmail.com>
2019-09-25 22:28:05 -0300

    no message

--- saldoapp/models/models.py
+++ saldoapp/models/models.py
@@ -17,17 +17,17 @@ class Movimiento(models.Model):
     _name = "sa.movimiento" # Nombre de la base de datos sa_movimiento (SQL)
     _description = "Movimiento" # Nombre del modelo en Odoo 'Movimiento' 
 
-    name = fields.Char(string="Concepto",size=150,index = True)
+    name = fields.Char(string="Nombre",size=150,index = True)
     #monto = fields.Char("Monto")
     monto_total = fields.Float(string = "Monto")
-    tipo = fields.Selection(string="Tipo",selection=[('I','Ingreso'),("E","Egreso")], default="I")
+    tipo = fields.Selection(string="Tipo",selection=[('I','Ingreso'),("E","Egreso")],default="I",required=True)
     fecha = fields.Date(string="Fecha de Operción")
     moneda = fields.Selection(string="Moneda",selection=[("PEN","Soles"),("USD","Dólares")])
-
     categoria_id = fields.Many2one("sa.categoria")
-    concepto = fields.Html("Concepto")
-    comprobante = fields.Binary("Comprobante")
-    puntos = fields.Integer("Puntos", related="categoria_id.puntos")
+    
+    concepto = fields.Html(string="Concepto")
+    comprobante = fields.Binary(string="Comprobante")
+    puntos = fields.Integer("Puntos",related="categoria_id.puntos")
 
 class Categoria(models.Model):
     _name = "sa.categoria"
@@ -38,4 +38,3 @@ class Categoria(models.Model):
     tipo = fields.Selection(string="Tipo",selection=[('I','Ingreso'),("E","Egreso")])
     active = fields.Boolean(string="Activo",default=True)
     puntos = fields.Integer("Puntos")
-
